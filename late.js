23f2a12b3722f73fa83900af7c02a288a60d6ad8
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-06-23 18:17:29 -0500

    added requirements

--- late.js
+++ late.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+
+function running_late(date) {
+	if(date.indexOf("PM") != -1) {
+		let getHour = date.substring(0, date.indexOf(':'));
+		if(10 <= parseInt(getHour) && 12 >= parseInt(getHour)){
+			return "It is late!";
+		}
+	}
+	return "It is still early!";
+}
+
+exports.running_late = running_late();
+
