715b4b3ffd7f63ddd8c82e799396d40ebb457f0d
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-06-23 19:08:12 -0500

    finally figured

--- late.js
+++ late.js
@@ -1,11 +1,9 @@
 // start coding your function here!
 
 function running_late(date) {
-	if(date.indexOf("PM") != -1) {
-		let getHour = parseInt(date.substring(date.indexOf(':')-2, date.indexOf(':')));
-		if(10 <= parseInt(getHour) && 12 >= parseInt(getHour)){
-			return "It is late!";
-		}
+	let getHour = parseInt(date.substring(date.indexOf(':')-2, date.indexOf(':')));
+	if(22 <= parseInt(getHour) && 24 >= parseInt(getHour)){
+		return "It is late!";
 	}
 	return "It is still early!";
 }
