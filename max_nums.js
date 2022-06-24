95027b394353287f38a2679ad3185bc5e806a86c
Batbuyan Nyamdavaa <bnyamd2@uic.edu>
2022-06-23 19:22:36 -0500

    ez?

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,12 @@
 // start coding your function here!
+
+function find_max(arr) {
+    let maxEl = Number.MIN_SAFE_INTEGER;
+    arr.forEach(element => {
+        if(element > maxEl) {
+            maxEl = element;
+        }
+    });
+    return maxEl;
+}
+exports.find_max = find_max;
